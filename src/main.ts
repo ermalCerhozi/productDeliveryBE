4dab3aed451587f522ed0c8379184a8762fe2346
ermalCe <ecerhozi@helius-systems.com>
2023-06-22 09:38:09 +0200

    feature: Added new fields to Product entity and declared databaaseQuery.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//TODO: Restrict which origins are allowed by passing an options object to enableCors().
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // this enables CORS
   await app.listen(3000);
 }
 bootstrap();
--- src/products/product.entity.ts
+++ src/products/product.entity.ts
@@ -10,4 +10,13 @@ export class Product {
 
   @Column('decimal', { precision: 8, scale: 2 })
   price: number;
+
+  @Column('text', { nullable: true })
+  description: string;
+
+  @Column({ length: 255, nullable: true })
+  image: string;
+
+  @Column('text', { nullable: true })
+  ingredients: string;
 }
